35de2969cdf73c6d2c5dc85801387a157946abbc
janissary1 <bellissimo38@gmail.com>
2019-10-20 23:11:49 -0400

    Fixed modal, added file upload JS, need to do file processing

--- eplus/eplus_main/static/eplus_main/scripts/index.js
+++ eplus/eplus_main/static/eplus_main/scripts/index.js
@@ -1,9 +1,22 @@
 function openModal(modal){
     document.getElementById(modal).style.display="flex";
-    document.getElementById("file_name").innerHTML = document.getElementById("myfile").files.item(0).name;
+    var file_name = document.getElementById("myfile").files.item(0).name;
+    document.getElementById("file_name").innerHTML = file_name;
+    
+    if (!file_name.endsWith(".zip")){
+        document.getElementById("compilebtn").setAttribute("disabled",true);
+        document.getElementById("error_msg").style.display="flex";
+    }
 }
 function closeModal(modal){
     document.getElementById(modal).style.display="none";
-    document.getElementById("myfile").value = null;
+    document.getElementById("error_msg").style.display="none";
+    document.getElementById("myfile").value = null; //reset the file selection
+    document.getElementById("compilebtn").removeAttribute("disabled");
 
+}
+
+function submit(){
+    alert(document.getElementById("myfile").value);
+    document.getElementById("upload").submit();
 }
--- eplus/eplus_main/views.py
+++ eplus/eplus_main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,loader
+from django.shortcuts import render,loader,redirect
 from django.http import HttpResponse
+import time as t
 
 #kek
 # Create your views here.
@@ -8,8 +9,12 @@ from django.http import HttpResponse
 def index(request):
     # template = loader.get_template('/file_upload.html')
      return render(request, 'eplus_main/file_upload.html')
-'''
+
 def process_zip(request):
     #unzip files
     #return list of files with a path name/folder representing where they  are located
-'''
+    if request.method == "POST": #request is valid
+        return HttpResponse("Files uploaded: {}".format(request.FILES["zip_file"].name))
+
+    else:
+        return HttpResponse('Error invalid request', status=403)
